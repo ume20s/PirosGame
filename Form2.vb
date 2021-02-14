4111ec32d6268809bdc0f729af496f157fdffb40
UMEDA Mitsuru <57313386+ume20s@users.noreply.github.com>
2021-02-14 10:36:31 +0900

    Add files via upload
    難易度セレクト画面

---
+++ Form2.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If RadioButton1.Checked = True Then
+            Form1.Timer2.Interval = 5
+            Form1.LevelLabel.Text = "HARD"
+            Form1.LevelLabel.Left = 290
+        ElseIf RadioButton2.Checked = True Then
+            Form1.Timer2.Interval = 50
+            Form1.LevelLabel.Text = "NORMAL"
+            Form1.LevelLabel.Left = 260
+        Else
+            Form1.Timer2.Interval = 500
+            Form1.LevelLabel.Text = "EASY"
+            Form1.LevelLabel.Left = 290
+        End If
+        Form1.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
